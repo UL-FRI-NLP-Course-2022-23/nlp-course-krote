24c05c346b800ee690f2b380ab76474a87b08ad8
Ziga Patacko Koderman <ziga@zerodays.dev>
2023-05-01 09:57:14 +0200

    update training

--- training/prompt.py
+++ training/prompt.py
@@ -7,12 +7,20 @@ model_name = sys.argv[1]
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
 
-comma_token_id = tokenizer.encode(',', add_special_tokens=False)[0]
+comma_token_id = tokenizer.encode(",", add_special_tokens=False)[0]
 
 while True:
     prompt = input("Enter your prompt: ")
     input_ids = tokenizer.encode(prompt, return_tensors="pt")
     attention_mask = torch.ones(input_ids.shape, dtype=torch.long, device=model.device)
-    output = model.generate(input_ids, attention_mask=attention_mask, max_length=128, do_sample=True, early_stopping=True, eos_token_id=comma_token_id)
-    responses = tokenizer.decode(output[0], skip_special_tokens=True).split('.')
-    print(responses[0] + '.')
+    output = model.generate(
+        input_ids,
+        attention_mask=attention_mask,
+        max_length=128,
+        do_sample=True,
+        early_stopping=True,
+        eos_token_id=comma_token_id,
+    )
+    responses = tokenizer.decode(output[0], skip_special_tokens=True).split(".")
+    print(responses[0] + ".")
+    print(responses[1] + ".")
--- training/train.py
+++ training/train.py
@@ -5,7 +5,7 @@ from transformers import (
     DataCollatorForLanguageModeling,
     Trainer,
     AutoConfig,
-    EvalPrediction
+    EvalPrediction,
 )
 from datasets import load_metric
 from sklearn.model_selection import train_test_split
@@ -14,7 +14,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from sklearn.metrics.pairwise import cosine_similarity
 
-
 from datasets import Dataset
 import torch
 import sys
@@ -22,9 +21,20 @@ import json
 
 from pynvml import *
 
+from datasets import load_metric
+
+rouge = load_metric("rouge")
+
+
+def compute_metrics(eval_pred: EvalPrediction):
+    predictions, labels = eval_pred
+    rouge_scores = rouge.compute(predictions=predictions, references=labels)
+    return rouge_scores
+
 
 dataset_path = sys.argv[1]
 model_name = sys.argv[2]
+
 
 def print_gpu_utilization():
     nvmlInit()
@@ -32,81 +42,59 @@ def print_gpu_utilization():
     info = nvmlDeviceGetMemoryInfo(handle)
     print(f"GPU memory occupied: {info.used//1024**2} MB.")
 
-def compute_metrics(eval_pred: EvalPrediction):
-    predictions, labels = eval_pred
-    perplexity = torch.exp(torch.tensor(predictions.loss))
-    return {"perplexity": perplexity}
 
 def tokenize_function(examples):
     return tokenizer(
         examples["input"], padding="max_length", truncation=True, max_length=128
     )
 
-class CustomLossWrapper(nn.Module):
-    def __init__(self, model, repetition_penalty):
-        super().__init__()
-        self.model = model
-        self.repetition_penalty = torch.tensor(repetition_penalty, device=model.device)
-
-    def forward(self, input_ids, labels=None, **kwargs):
-        outputs = self.model(input_ids, labels=labels, **kwargs)
-        loss = outputs[0]
-
-        if self.repetition_penalty > 1:
-            input_tokens = input_ids[:, -1]
-            for token in input_tokens:
-                token_count = (input_ids == token).sum(dim=1).clamp(1)
-                penalty = (token_count * torch.log(self.repetition_penalty)).sum()
-                loss += penalty
-
-        return loss, outputs[1:]
-
-
 
 tokenizer = AutoTokenizer.from_pretrained("cjvt/gpt-sl-base")
 print_gpu_utilization()
-model = AutoModelWithLMHead.from_pretrained("cjvt/gpt-sl-base").to('cuda')
-model = CustomLossWrapper(model, 1.2)
+model = AutoModelWithLMHead.from_pretrained("cjvt/gpt-sl-base").to("cuda")
 print_gpu_utilization()
 
 tmp = []
 with open(dataset_path, "r") as f:
     tmp = json.load(f)
 
-# tmp = tmp[:100]
-
 tmp = list(
     filter(lambda x: x["paraphrased"] is not None and x["original"] is not None, tmp)
 )
 tmp_train, tmp_eval = train_test_split(tmp, test_size=0.2)
+
+# Modify the input format to include the "paraphrase" prompt
 train_data = {
-    "input": [x["original"] for x in tmp_train],
-    "output": [x["paraphrased"] for x in tmp_train],
+    "input": ["Originalno: " + x["original"] for x in tmp_train],
+    "output": ["Parafrazirano: " + x["paraphrased"] for x in tmp_train],
 }
 eval_data = {
-    "input": [x["original"] for x in tmp_eval],
-    "output": [x["paraphrased"] for x in tmp_eval],
+    "input": ["Originalno: " + x["original"] for x in tmp_eval],
+    "output": ["Parafrazirano: " + x["paraphrased"] for x in tmp_eval],
 }
 
 train_dataset = Dataset.from_dict(train_data)
 eval_dataset = Dataset.from_dict(eval_data)
 print(len(train_data["input"]), len(eval_data["input"]))
 
-
 tokenized_train_dataset = train_dataset.map(tokenize_function, batched=True)
 tokenized_eval_dataset = eval_dataset.map(tokenize_function, batched=True)
+
+# Increase the number of training epochs
+num_epochs = 30
+
 training_args = TrainingArguments(
     output_dir="./output",
-    evaluation_strategy="epoch",
-    learning_rate=1e-5,
-    per_device_train_batch_size=3,
-    per_device_eval_batch_size=3,
-    num_train_epochs=20,
-    weight_decay=1e-3,
+    evaluation_strategy="no",
+    learning_rate=1e-4,
+    per_device_train_batch_size=1,
+    per_device_eval_batch_size=1,
+    num_train_epochs=num_epochs,
+    weight_decay=1e-2,
     load_best_model_at_end=True,
-    metric_for_best_model="perplexity",
-    greater_is_better=False,
-    save_strategy="epoch",
+    metric_for_best_model="rougeL",  # Choose a specific ROUGE variant
+    greater_is_better=True,
+    save_strategy="no",
 )
 
 data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
@@ -122,5 +110,5 @@ trainer = Trainer(
 
 trainer.train()
 
-trainer.model.model.save_pretrained(model_name)
+trainer.model.save_pretrained(model_name)
 tokenizer.save_pretrained(model_name)
